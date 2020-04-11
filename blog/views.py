cf36996292d10b3cecbed06d6312323f1d15f5a9
tamal <tamal.sen@hotmail.com>
2020-04-11 20:05:20 +0400

    ready for heroku

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView , DetailView
+from django.views.generic.edit import CreateView , UpdateView ,DeleteView
+from django.urls import reverse_lazy
 
 from . models import BlogPost
 
@@ -11,3 +13,22 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
 	model = BlogPost
 	template_name = 'post_detail.html'
+
+
+class BlogCreateView(CreateView):
+	model = BlogPost
+	template_name = 'post_new.html'
+	fields = '__all__'
+
+class BlogUpdateView(UpdateView):
+	model = BlogPost
+	template_name = 'post_edit.html'
+	fields = ['title' , 'title_body']
+	
+class BlogDeleteView(DeleteView):
+	model = BlogPost
+	template_name = 'post_delete.html'
+	success_url = reverse_lazy('home')
+
+
+
--- blog_project/settings.py
+++ blog_project/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -38,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'blog_project.urls'
@@ -122,3 +127,10 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS= [
     os.path.join(BASE_DIR,'static')
 ]
+
+STATICFILES_STORAGE = 'Whitenoise.storage.CompressedManifest.static'
+django_heroku.settings(locals())
+
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
